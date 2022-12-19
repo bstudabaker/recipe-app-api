57f033add6ff124b142ff1c45722416dd98e156d
bstudabaker <bryan.studabaker@gmail.com>
2022-12-19 17:45:23 -0500

    Fixed linting errors

--- app/app/calc.py
+++ app/app/calc.py
@@ -5,3 +5,4 @@ Calculator Functions
 def add(x, y):
     """Adds x and y and returns the result"""
     return x + y
+
--- app/app/tests.py
+++ app/app/tests.py
@@ -12,3 +12,4 @@ class CalcTests(SimpleTestCase):
     def test_add_numbers(self):
         res = add(5, 6)
         self.assertEqual(res, 11)
+
